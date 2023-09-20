f21030511a7ed26cc8cfcedd5f2d837db0f617dc
alexandertian <alex2262alexander@gmail.com>
2023-09-20 15:59:36 -0400

    changes!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/CVSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/CVSubsystem.java
@@ -1,12 +1,68 @@
 package org.firstinspires.ftc.teamcode.subsystems;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+
+import java.util.List;
 
 public class CVSubsystem {
     public final int LOCATION_LEFT = 0;
     public final int LOCATION_CENTER = 1;
     public final int LOCATION_RIGHT = 2;
 
-    public CVSubsystem() {
+    private AprilTagProcessor aprilTag;
+    private VisionPortal visionPortal;
+
+    public CVSubsystem(OpenCvCamera camera) {
         // create AprilTagProcessor and VisionPortal
+
+        initAprilTag();
+    }
+
+    private void initAprilTag() {
+
+        // Create the AprilTag processor.
+        aprilTag = new AprilTagProcessor.Builder()
+                .setDrawTagOutline(true)
+                .build();
+
+        // to modify, look for the specs in ConceptAprilTag.java:
+        //.setDrawAxes(false)
+        //.setDrawCubeProjection(false)
+        //.setDrawTagOutline(true)
+        //.setTagFamily(AprilTagProcessor.TagFamily.TAG_36h11)
+        //.setTagLibrary(AprilTagGameDatabase.getCenterStageTagLibrary())
+        //.setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
+        // Create the vision portal by using a builder.
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+
+
+        // Set the built-in RC phone camera
+        builder.setCamera(BuiltinCameraDirection.BACK);
+        builder.setAutoStopLiveView(false); // keep camera on when not processing
+
+//      //builder.setCameraResolution(new Size(640, 480));
+        // Choose a camera resolution. Not all cameras support all resolutions.
+
+        // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
+        //builder.enableCameraMonitoring(true);
+
+        // Choose whether or not LiveView stops if no processors are enabled.
+        // If set "true", monitor shows solid orange screen if no processors enabled.
+        // If set "false", monitor shows camera view without annotations.
+        //builder.setAutoStopLiveView(true);
+
+        // Set and enable the processor.
+        builder.addProcessor(aprilTag);
+
+        // Build the Vision Portal, using the above settings.
+        visionPortal = builder.build();
+
+        // Disable or re-enable the aprilTag processor at any time.
+        // visionPortal.setProcessorEnabled(aprilTag, true);
 
     }
 
@@ -16,6 +72,37 @@ public class CVSubsystem {
      * @return whether the AprilTag is left, center, or right in the camera view
      */
     public int getAprilTagLocation(int tagID) {
+
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        // telemetry.addData("# AprilTags Detected", currentDetections.size());
+
+        // Step through the list of detections and display info for each one.
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+
+                // AprilTagOutput = detection.id;
+
+                /*
+                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+                */
+            } else {
+                /*
+                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+                */
+            }
+        }   // end for() loop
+
+        // Add "key" information to telemetry
+        /*
+        telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
+        telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+        telemetry.addLine("RBE = Range, Bearing & Elevation");
+        */
+
         return 0; // TEMPORARY
     }
 
