ba61db773293546eefcbee61e19aeb29008a7cf9
adazing <109737747+adazing@users.noreply.github.com>
2023-10-03 23:35:57 -0400

    Update PropDetectionPipeline.java
    finds center point and stuff.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/cvpipelines/PropDetectionPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/cvpipelines/PropDetectionPipeline.java
@@ -11,11 +11,16 @@ import org.openftc.easyopencv.OpenCvPipeline;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 
 // NOTE: changing this code very soon :)
 public class PropDetectionPipeline extends OpenCvPipeline {
+    Telemetry telemetry;
+
     public Mat frame;
     public Mat sub;
     public Mat temp = new Mat();
@@ -31,9 +36,13 @@ public class PropDetectionPipeline extends OpenCvPipeline {
 
     public int contour_dim_ratio;
 
-    // ADA's idea: COMPARE RATIOS OF WIDTH : HEIGHT TO BE MORE ACCURATE ON DETECTING TAPE AND POLE
+    public PropDetectionPipeline(Telemetry telemetry){
+        this.telemetry = telemetry;
+    }
+
     @Override
     public Mat processFrame(Mat input) {
+        this.telemetry = telemetry;
         hasStarted.set(true);
         //open cv defaults to bgr, but we are completely in rgb/hsv, this is because pipeline's input/output rgb
         height = input.height()/3;
@@ -41,50 +50,70 @@ public class PropDetectionPipeline extends OpenCvPipeline {
         int y = 2 * height;
         int x = 0;
         this.sub = input;
-        this.frame = input;
-        int posy = height/2;
-        int posx = width / 2;
-        MatOfPoint contour = getConeContour();
-        if(contour != null) {
-            List<MatOfPoint> coneContour = new ArrayList<>();
+//        int posy = height/2;
+//        int posx = width / 2;
+        MatOfPoint contour = getMaxContour();
+        if (contour != null) {
+            Imgproc.drawContours(sub, new ArrayList<MatOfPoint>(Arrays.asList(contour)), 0, new Scalar(255, 255, 0), 2);
             Rect b = Imgproc.boundingRect(contour);
-//            contour_dim_ratio=b.width/b.height;
-//            if (contour_dim_ratio < 1) {
-            coneContour.add(contour);
-            Imgproc.drawContours(sub, coneContour, 0, new Scalar(255, 255, 0), 2);
-            posx = b.x + b.width / 2;
-            posy = b.y + b.height / 2;
-            Imgproc.circle(sub, new Point(posx, posy), 2, new Scalar(255, 255, 0)); //rgb DRAW CIRCLE
-//            }
-
+            Imgproc.rectangle(sub, b, new Scalar(255, 255, 0));
+            Point center = new Point(b.x + b.width/2, b.y + b.height/2);
+            Imgproc.circle(sub, center, 2, new Scalar(255, 255, 0));
+            telemetry.addData("x",b.x + b.width/2);
+            telemetry.addData("y",b.y + b.height/2);
+            telemetry.update();
         }
+//        telemetry.addData("foo", 0);
+        telemetry.addData("height", height);
+        telemetry.addData("width", width);
+        telemetry.update();
+//
+//        telemetry.addData("height", height);
+//        telemetry.addData("width", width);
 
-        int offset = posx - (width / 2 + 40);
-        lateralOffset.set(offset);
+//        if(contour != null) {
+////            List<MatOfPoint> coneContour = new ArrayList<>();
+//            Rect b = Imgproc.boundingRect(contour);
+////            contour_dim_ratio=b.width/b.height;
+////            if (contour_dim_ratio < 1) {
+//            coneContour.add(contour);
+//            Imgproc.drawContours(sub, coneContour, 0, new Scalar(255, 255, 0), 2);
+//            posx = b.x + b.width / 2;
+//            posy = b.y + b.height / 2;
+//            Imgproc.circle(sub, new Point(posx, posy), 2, new Scalar(255, 255, 0)); //rgb DRAW CIRCLE
+////            }
+
+//        }
+
+//        int offset = posx - (width / 2 + 40);
+//        lateralOffset.set(offset);
 
         return input;
     }
 
-    public MatOfPoint getConeContour() {
+    public MatOfPoint getMaxContour() {
         Mat hsvmat = new Mat();
         Imgproc.cvtColor(sub, hsvmat, Imgproc.COLOR_RGB2HSV);
 
         Mat inRange = new Mat();
         Core.inRange(hsvmat, lowerRange, upperRange, inRange);
 
-        List<MatOfPoint> blueContourList = new ArrayList<>();
-        Imgproc.findContours(inRange, blueContourList, temp, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+        List<MatOfPoint> contourList = new ArrayList<>();
+        Imgproc.findContours(inRange, contourList, temp, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 
         double maxArea = Double.MIN_VALUE;
-        MatOfPoint maxContour = null;
-        for (MatOfPoint m : blueContourList) {
-            double area = Imgproc.contourArea(m);
+        int best_idx = -1;
+        for (int idx = 0; idx<contourList.size(); idx++) {
+            double area = Imgproc.contourArea(contourList.get(idx));
             if (area > maxArea) {
                 maxArea = area;
-                maxContour = m;
+                best_idx = idx;
             }
         }
-
-        return maxContour;
+        if (best_idx!=-1) {
+            return contourList.get(best_idx);
+        }else{
+            return null;
+        }
     }
 }
