1262185b9fb68d4a9b2a474c3a6c0454419b2f4a
Naval <miao20090403@gmail.com>
2023-10-11 16:16:08 -0400

    contour

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/cvpipelines/TestPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/cvpipelines/TestPipeline.java
@@ -10,18 +10,20 @@ import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TestPipeline extends OpenCvPipeline {
     private Mat frame = new Mat();
-    public boolean beforeTape = false;
+    public int beforeTape = 0;
+    public double largestArea_=0;
     @Override
     public Mat processFrame(Mat input) {
         Imgproc.rectangle(input,new Rect(0, 330, 640, 100), new Scalar(255, 0, 0), 2);
         Rect crop = new Rect(0, 330, 640, 100);
         Mat cropped = input.submat(crop);
         frame = cropped;
-        beforeTape = isBeforeTape(true);
+        beforeTape = isBeforeTape(false) ? 1 : 0;
         return frame;
 
     }
@@ -30,16 +32,21 @@ public class TestPipeline extends OpenCvPipeline {
         Mat hsvMat = new Mat();
         Imgproc.cvtColor(frame, hsvMat, Imgproc.COLOR_RGB2HSV);
         Mat inRange = new Mat();
-        double largestArea = Double.MIN_VALUE;
+        double largestArea = 0;
         Core.inRange(hsvMat, new Scalar(100, 90, 80), new Scalar(125, 255, 255), inRange);
         List<MatOfPoint> contourList = new ArrayList<>();
         Imgproc.findContours(inRange, contourList, new Mat(), Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
         for (int i = 0; i < contourList.size(); i++) {
             Imgproc.drawContours(frame, contourList, i, new Scalar(0, 255, 0), 1);
-            contourList.get(i); //all contour size list
+            MatOfPoint result = contourList.get(i); //all contour size list
+            double area = Imgproc.contourArea(result);
+            if (area > largestArea) {
+                largestArea = area;
+            }
             //get area of largest contour
             //set largestArea to that
         }
+        largestArea_ = largestArea;
         if (largestArea > areaThreshold) {
             return true;
         } else {
