4ea401871b8b5aa6d10d91219b2e8da551b5f970
natnuo <--global>
2023-12-01 21:14:11 -0500

    lol we didn't test red tlteope

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/DriveTeleOpRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/DriveTeleOpRed.java
@@ -1,24 +1,15 @@
 package org.firstinspires.ftc.teamcode.opmodes.teleop;
 
-import com.arcrobotics.ftclib.gamepad.GamepadEx;
-import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.checkerframework.checker.units.qual.Speed;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.teamcode.HardwareRobot;
-import org.firstinspires.ftc.teamcode.SpeedCoefficients;
-import org.firstinspires.ftc.teamcode.subsystems.InDepSubsystem;
-import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
-import org.firstinspires.ftc.teamcode.subsystems.CVSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.TeleOpUtil;
 
-@TeleOp(name = "BLUE Drive TeleOp")
-public class DriveTeleOp extends LinearOpMode {
+@TeleOp(name = "RED Drive TeleOp")
+public class DriveTeleOpRed extends LinearOpMode {
     @Override
     public void runOpMode() {
-        TeleOpUtil teleOp = new TeleOpUtil(hardwareMap, telemetry, false, gamepad1, gamepad2, this);
+        TeleOpUtil teleOp = new TeleOpUtil(hardwareMap, telemetry, true, gamepad1, gamepad2, this);
         waitForStart();
         while (opModeIsActive()) {
             teleOp.tick();
