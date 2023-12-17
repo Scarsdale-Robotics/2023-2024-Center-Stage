376a8122cd2ae0599e10a7670306c74c07006d7d
Stephen Cai <KK201431873@gmail.com>
2023-12-16 22:25:08 -0500

    changed to ExecutorService for safer multithreading (hopefully)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/movement/MovementThread.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/movement/MovementThread.java
@@ -18,19 +18,20 @@ public class MovementThread implements Runnable {
     private static final double POWER_TURN = SpeedCoefficients.getAutonomousTurnSpeed();
     private static final double POWER_ARM = SpeedCoefficients.getAutonomousArmSpeed();
 
-    final private ElapsedTime runtime;
-    final private Movement movement;
-    final private DriveSubsystem drive;
-    final private InDepSubsystem inDep;
-    final private LinearOpMode opMode;
+    private final ElapsedTime runtime;
+    private final Movement movement;
+    private static volatile DriveSubsystem drive;
+    private static volatile InDepSubsystem inDep;
+    private static volatile LinearOpMode opMode;
 
     public MovementThread(Movement movement, DriveSubsystem drive, InDepSubsystem inDep, LinearOpMode opMode) {
         this.movement = movement;
-        this.drive = drive;
-        this.inDep = inDep;
-        this.opMode = opMode;
         runtime = new ElapsedTime();
         runtime.reset();
+
+        MovementThread.drive = drive;
+        MovementThread.inDep = inDep;
+        MovementThread.opMode = opMode;
     }
 
     @Override
