c4123937d3554ad4911088ed9ee43c08739d5f65
natnuo <--global>
2024-02-12 16:53:31 -0500

    gamepad 2 dpad down slow mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/TeleOpUtil.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/TeleOpUtil.java
@@ -237,6 +237,8 @@ public class TeleOpUtil {
             resetArmEncoderToggle = true;
         }
         if (!gamepad2.circle && !gamepad2.a) resetArmEncoderToggle = false;
+        if (gamepad2.dpad_down) SpeedCoefficients.setMode(SpeedCoefficients.MoveMode.MODE_SLOW);
+        else SpeedCoefficients.setMode(SpeedCoefficients.MoveMode.MODE_FAST);
     }
 
 
